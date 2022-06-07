930f205acfd29f6828bc4974ff244968a5841301
Andrew Patrick Ong Tolentino <atolentino@hawk.iit.edu>
2022-06-06 21:42:19 -0500

    Turn into arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import RootLayout from './shared/layouts/root-layout.component';
+import RootLayout from './shared/layouts/root-layout/root-layout.component';
 import ReviewsPage from './pages/reviews-page.component';
 import ReviewPage from './pages/review-page.component';
 
-function App() {
+const App = () => {
   return (
     <Routes>
       <Route path="/" element={<RootLayout />}>
